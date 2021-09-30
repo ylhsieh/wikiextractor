b35e863c2e687ab6b3dfb65a33e5a79fe3e35fde
morpheus <morpheus.hsieh@mile.cloud>
2021-09-30 02:05:37 +0000

    increase output file size

--- get-zhwiki.sh
+++ get-zhwiki.sh
@@ -1,2 +1,2 @@
-python wikiextractor/cirrus-extract-notags.py -b 100M -o ooooo  zhwiki-20210809-cirrussearch-content.json.gz
+python wikiextractor/cirrus-extract-notags.py -b 1G -o extracted  zhwiki-20210809-cirrussearch-content.json.gz
 
